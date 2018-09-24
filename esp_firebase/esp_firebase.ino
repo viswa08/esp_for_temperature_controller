1647abddd4541b6eab17da61762e6258adfc47a6
Viswanath N <viswa0894@gmail.com>
2018-09-24 15:45:30 +0530

    stable code

--- esp_firebase/esp_firebase.ino
+++ esp_firebase/esp_firebase.ino
@@ -51,20 +51,62 @@ void setup() {
 }
 void loop() 
 {
-    ReadTheData ="";
+  
+    ReadIncomingData();
+    delay(1000);
+      if(CurrentTemperature > 4.00)
+      {
+        Serial.print("temperature = ");Serial.println(CurrentTemperature); 
+        Firebase.pushFloat("sensor1/temperature",CurrentTemperature);
+      }
+      else{
+        Serial.println("garbage value");
+      }
+      if(CurrentVoltage>4.00){
+        Serial.print("voltage = ");Serial.println(CurrentVoltage);
+        Firebase.pushFloat("sensor1/voltage",CurrentVoltage);
+      }
+      else{
+        Serial.println("voltage garbage");
+      }
+    
+      
+}
+
+void ReadIncomingData() {
+      ReadTheData ="";
        while(Serial.available()){
             char b =  Serial.read();
             ReadTheData += b; //Wire.read();
-            Serial.print(ReadTheData);
        }
 
           if (ReadTheData.indexOf("TEMPA:") >=0) {
                          T1 = ReadTheData.substring(6,8);
                          T2 = ReadTheData.substring(8,10);
                          CurrentTemperature = (T1.toFloat()*100 + T2.toFloat())/100;
-                         Serial.println(CurrentTemperature);
           }
-           
+           else if (ReadTheData.indexOf("VOLTS:") >=0) {
+                         T1 = ReadTheData.substring(6,9);
+                         CurrentVoltage = T1.toFloat();
+          }
+          else if (ReadTheData.indexOf("AMPS :") >=0) {
+                         T1 = ReadTheData.substring(6,9);
+                         AcCurrent = T1.toFloat();
+          }
+          else if (ReadTheData.indexOf("MIN-EEPROM:") >=0) {
+                  T1 = ReadTheData.substring(11,13);
+                  T2 = ReadTheData.substring(13,15);
+                  EEPROMMinValue = (T1.toFloat()*100 + T2.toFloat())/100;
+          }
+          else if (ReadTheData.indexOf("MAX-EEPROM:") >=0) {
+                  T1 = ReadTheData.substring(11,13);
+                  T2 = ReadTheData.substring(13,15);
+                   EEPROMMaxValue = (T1.toFloat()*100 + T2.toFloat())/100;
+          }
+                       /*if(CurrentTemperature > 27)
+                          {
+                          thing.call_endpoint("temp_point");
+                          }*/
 }
 
 

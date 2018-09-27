8439b4a916adc3bed183e9db8d474b5dcb128e06
Viswanath N <viswa0894@gmail.com>
2018-09-27 14:43:42 +0530

    bug correction

--- esp_firebase/esp_firebase.ino
+++ esp_firebase/esp_firebase.ino
@@ -19,7 +19,7 @@
 //#define WIFI_PASSWORD "sree2930"
 
 float reading;
-float CurrentTemperature, CurrentVoltage, AcCurrent;
+float CurrentTemperature, CurrentVoltage, AcCurrent,BugTemperature;
 int test=25;
 float val;
 float MIN,MAX;
@@ -28,13 +28,14 @@ float CLOUDMinValue, CLOUDMaxValue;
 float Value;
 int RData;
 
-String ReadTheData,T1,T2;
+String ReadTheData,T1,T2,T1_bug,T2_bug;
 char SendTheData[300];
 
 float CurrentMin, CurrentMax; 
 
 //OneWire oneWire(ONE_WIRE_BUS);
 //DallasTemperature sensors(&oneWire);
+
 
 void setup() {
 
@@ -51,16 +52,17 @@ void setup() {
 }
 void loop() 
 {
-  
-    ReadIncomingData();
     delay(750);
-      if(CurrentTemperature > 4.00)
+    ReadIncomingData();
+    
+      if((CurrentTemperature > 4.00)&&(CurrentTemperature != BugTemperature))
       {
-        //Serial.print("temperature = ");Serial.println(CurrentTemperature); 
+        Serial.print("temperature = ");Serial.println(CurrentTemperature);
+        Serial.print("Bug = ");Serial.println(BugTemperature);
         Firebase.pushFloat("sensor1/temperature",CurrentTemperature);
       }
       else{
-        Serial.println("garbage value");
+        //Serial.println("garbage value");
       }
       if(CurrentVoltage>4.00){
         //Serial.print("voltage = ");Serial.println(CurrentVoltage);
@@ -69,28 +71,46 @@ void loop()
       else{
         //Serial.println("voltage garbage");
       }
-      Firebase.pushFloat("sensor1/Amps",AcCurrent);
+      if(AcCurrent>0.00)
+      {
+        Firebase.pushFloat("sensor1/Amps",AcCurrent);
+      }
+      
       
       //Get Minimum and maximum values from cloud
-      CLOUDMinValue = Firebase.getFloat("sensor1/minimum temperature");
-      CLOUDMaxValue = Firebase.getFloat("sensor1/maximum temperature");   
+      CLOUDMinValue = Firebase.getInt("sensor1/minimum temperature");
+      CLOUDMaxValue = Firebase.getInt("sensor1/maximum temperature");   
       //Serial.print("Min value = ");Serial.println(CLOUDMinValue);
       delay(100);
-      if (CLOUDMinValue !=0.00) {
+      if (CLOUDMinValue !=0) {
                   //Serial.println("setting minimum value");
+                  
                   T1 = "MINTEMP:";
-                  T1+=CLOUDMinValue;
+                  T2 = (int)CLOUDMinValue*100;
+                  T1+=T2;
                   T1.toCharArray(SendTheData,300);
                   Serial.write(SendTheData);
+                  //Serial.print("MINTEMP= ");Serial.println(SendTheData);
                   delay(100);
              }
              if (CLOUDMaxValue !=0) {
+              
                   T1 = "MAXTEMP:";
-                  T1+=CLOUDMaxValue;
+                  T2 = (int)CLOUDMaxValue*100;
+                  T1+=T2;
                   T1.toCharArray(SendTheData,300);
                   Serial.write(SendTheData);
                   delay(100);
            }
+//           if(EEPROMMinValue != 0)
+//           {
+//            Serial.print("EEprom Min value = ");Serial.println(EEPROMMinValue);
+//           }
+//           if(EEPROMMaxValue != 0)
+//           {
+//            Serial.print("EEprom Max value = ");Serial.println(EEPROMMaxValue);
+//           }
+//           
 }
 
 void ReadIncomingData() {
@@ -101,13 +121,17 @@ void ReadIncomingData() {
        }
 
           if (ReadTheData.indexOf("TEMPA:") >=0) {
-                         T1 = ReadTheData.substring(6,8);
-                         T2 = ReadTheData.substring(8,10);
-                         CurrentTemperature = (T1.toFloat()*100 + T2.toFloat())/100;
+                       T1 = ReadTheData.substring(6,8);
+                       T2 = ReadTheData.substring(8,10);
+                       CurrentTemperature = (T1.toFloat()*100 + T2.toFloat())/100;
           }
            else if (ReadTheData.indexOf("VOLTS:") >=0) {
                          T1 = ReadTheData.substring(6,9);
+                         T1_bug = ReadTheData.substring(6,8);
+                         T2_bug = ReadTheData.substring(8,10);
                          CurrentVoltage = T1.toFloat();
+                         BugTemperature = (T1_bug.toFloat()*100 + T2_bug.toFloat())/100;
+                         
           }
           else if (ReadTheData.indexOf("AMPS :") >=0) {
                          T1 = ReadTheData.substring(6,9);

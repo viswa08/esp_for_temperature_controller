81c4aad759487b9f21d398a28ef1d8e2565c2f74
Viswanath N <viswa0894@gmail.com>
2018-10-01 13:56:54 +0530

    changes for stable limit values

--- esp_firebase_sensor2/esp_firebase/esp_firebase.ino
+++ esp_firebase_sensor2/esp_firebase/esp_firebase.ino
@@ -28,7 +28,7 @@ float CLOUDMinValue, CLOUDMaxValue;
 float Value;
 int RData;
 
-String ReadTheData,T1,T2,T1_bug,T2_bug;
+String ReadTheData,T1,T2,T1_bug,T2_bug,T3;
 char SendTheData[300];
 
 float CurrentMin, CurrentMax; 
@@ -49,70 +49,78 @@ void setup() {
    
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 
-  CLOUDMinValue = Firebase.setFloat("sensor1/minimum temperature", 0);
-  CLOUDMinValue = Firebase.setFloat("sensor1/maximum temperature", 0);
+  //Firebase.setFloat("sensor2/minimum temperature", 0);
+  //Firebase.setFloat("sensor2/maximum temperature", 0); 
+
+  
 }
 void loop() 
 {
+  
     delay(750);
     ReadIncomingData();
-    
-      if((CurrentTemperature > 4.00)&&(CurrentTemperature != BugTemperature))
-      {
-        Serial.print("temperature = ");Serial.println(CurrentTemperature);
-        Serial.print("Bug = ");Serial.println(BugTemperature);
-        Firebase.pushFloat("sensor1/temperature",CurrentTemperature);
-      }
-      else{
-        //Serial.println("garbage value");
-      }
-      if(CurrentVoltage>4.00){
-        //Serial.print("voltage = ");Serial.println(CurrentVoltage);
-        Firebase.pushFloat("sensor1/voltage",CurrentVoltage);
-      }
-      else{
-        //Serial.println("voltage garbage");
-      }
-      if(AcCurrent>0.00)
-      {
-        Firebase.pushFloat("sensor1/Amps",AcCurrent);
-      }
-      
-      
-      //Get Minimum and maximum values from cloud
-      CLOUDMinValue = Firebase.getFloat("sensor1/minimum temperature");
-      CLOUDMaxValue = Firebase.getFloat("sensor1/maximum temperature");   
-      //Serial.print("Min value = ");Serial.println(CLOUDMinValue);
-      delay(100);
-      if (CLOUDMinValue !=0.00) {
+    //Get Minimum and maximum values from cloud
+      CLOUDMinValue = Firebase.getInt("sensor2/minimum temperature");
+      //Serial.println(CLOUDMinValue);
+      CLOUDMaxValue = Firebase.getInt("sensor2/maximum temperature");   
+      //Serial.print("Max value = ");Serial.println(CLOUDMaxValue);
+      delay(200);
+      if (CLOUDMinValue !=0) {
                   //Serial.println("setting minimum value");
                   
                   T1 = "MINTEMP:";
                   T2 = (int)CLOUDMinValue*100;
-                  T1+=T2;
+                  T3 = "\n";
+                  T1 = T1+T2+T3;
                   T1.toCharArray(SendTheData,300);
                   Serial.write(SendTheData);
                   //Serial.print("MINTEMP= ");Serial.println(SendTheData);
                   delay(100);
              }
-             if (CLOUDMaxValue !=0.00) {
+             if (CLOUDMaxValue !=0) {
               
                   T1 = "MAXTEMP:";
                   T2 = (int)CLOUDMaxValue*100;
-                  T1+=T2;
+                  T3 = "\n";
+                  T1 = T1+T2+T3;
                   T1.toCharArray(SendTheData,300);
                   Serial.write(SendTheData);
                   delay(100);
            }
-//           if(EEPROMMinValue != 0)
-//           {
-//            Serial.print("EEprom Min value = ");Serial.println(EEPROMMinValue);
-//           }
+//    
+//      if((CurrentTemperature > 4.00)&&(CurrentTemperature != BugTemperature))
+//      {
+//        Serial.print("temperature = ");Serial.println(CurrentTemperature);
+//        Serial.print("Bug = ");Serial.println(BugTemperature);
+//        Firebase.pushFloat("sensor2/temperature",CurrentTemperature);
+//      }
+//      else{
+//        //Serial.println("garbage value");
+//      }
+//      if(CurrentVoltage>4.00){
+//        //Serial.print("voltage = ");Serial.println(CurrentVoltage);
+//        Firebase.pushFloat("sensor2/voltage",CurrentVoltage);
+//      }
+//      else{
+//        //Serial.println("voltage garbage");
+//      }
+//      if(AcCurrent>0.00)
+//      {
+//        Firebase.pushFloat("sensor2/Amps",AcCurrent);
+//      }
+      
+      
+//      
 //           if(EEPROMMaxValue != 0)
 //           {
 //            Serial.print("EEprom Max value = ");Serial.println(EEPROMMaxValue);
 //           }
-//           
+//           if(EEPROMMinValue != 0)
+//           {
+//            Serial.print("EEprom Min value = ");Serial.println(EEPROMMinValue);
+//           }
+           
+           
 }
 
 void ReadIncomingData() {
@@ -121,11 +129,17 @@ void ReadIncomingData() {
             char b =  Serial.read();
             ReadTheData += b; //Wire.read();
        }
+       
 
           if (ReadTheData.indexOf("TEMPA:") >=0) {
                        T1 = ReadTheData.substring(6,8);
                        T2 = ReadTheData.substring(8,10);
                        CurrentTemperature = (T1.toFloat()*100 + T2.toFloat())/100;
+                       //Serial.print("temperature");Serial.println(CurrentTemperature);
+                       if(CurrentTemperature > 10.00)
+                       {
+                        Firebase.pushFloat("sensor2/temperature",CurrentTemperature);
+                       }
           }
            else if (ReadTheData.indexOf("VOLTS:") >=0) {
                          T1 = ReadTheData.substring(6,9);
@@ -133,11 +147,17 @@ void ReadIncomingData() {
                          T2_bug = ReadTheData.substring(8,10);
                          CurrentVoltage = T1.toFloat();
                          BugTemperature = (T1_bug.toFloat()*100 + T2_bug.toFloat())/100;
+                         if(CurrentVoltage > 10.00)
+                         {
+                          Firebase.pushFloat("sensor2/voltage",CurrentVoltage);
+                         }
                          
           }
           else if (ReadTheData.indexOf("AMPS :") >=0) {
                          T1 = ReadTheData.substring(6,9);
                          AcCurrent = T1.toFloat();
+                         Firebase.pushFloat("sensor2/Amps",AcCurrent);
+                         
           }
           else if (ReadTheData.indexOf("MIN-EEPROM:") >=0) {
                   T1 = ReadTheData.substring(11,13);
